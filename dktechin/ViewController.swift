8b1dfa54ccdc53493754e36e05c71f0628888b47
SeungUkShin <krdut1@gmail.com>
2022-08-20 20:04:03 +0900

    [신승욱] 과제 진행중

--- dktechin/ViewController.swift
+++ dktechin/ViewController.swift
@@ -8,12 +8,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let imagePicker = UIImagePickerController()
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var heightConstant: NSLayoutConstraint!
+    @IBOutlet weak var widthConstant: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //imagePicker setup
+        self.imagePicker.sourceType = .photoLibrary
+        self.imagePicker.delegate = self // picker delegate
+        self.imagePicker.sourceType = .photoLibrary // 앨범에서 가져옴
+        self.imagePicker.allowsEditing = false // 수정 가능 여부
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        plusAction()
+    }
+    
+    func plusAction() {
+        let alert =  UIAlertController(title: "사진 추가 / 변경", message: nil, preferredStyle: .actionSheet)
 
+        let library =  UIAlertAction(title: "앨범", style: .default) {
+            (action) in self.openLibrary()
+        }
 
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(library)
+//        alert.addAction(camera)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func openLibrary(){
+        present(imagePicker, animated: false, completion: nil)
+    }
+    @IBAction func btnAction(_ sender: Any) {
+        let tagIndex = (sender as! UIButton).tag
+        
+        switch tagIndex {
+        case 0:
+            print("0")
+            break
+        case 1:
+            print("1")
+            break
+        case 2:
+            print("2")
+            break
+        case 3:
+            plusAction()
+            break
+        default:
+            print("default")
+        }
+    }
 }
 
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        var newImage: UIImage? = nil // update 할 이미지
+        
+        if let possibleImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            newImage = possibleImage // 원본 이미지가 있을 경우
+        }
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        widthConstant.constant = (newImage?.size.width)!
+        heightConstant.constant = (newImage?.size.height)!
+        
+        self.imageView.image = newImage // 받아온 이미지를 update
+        
+        
+        picker.dismiss(animated: true, completion: nil) // picker를 닫아줌
+    }
+}
